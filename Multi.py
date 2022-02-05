616e3d75a14ccf56eaff826d045d36485edb3770
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-02-05 17:23:09 +0300

    Update Multi.py

--- Multi.py
+++ Multi.py
@@ -7,8 +7,8 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from libvip import main
+    from oldnew import main
     main()
 elif bit == '32bit':
-    from libvip import main
+    from oldnew import main
     main()
