3a985f768c96d8f3680958a0e3b6e5efc1a66883
James404 <68128416+James404-cyber@users.noreply.github.com>
2023-02-26 10:29:14 +0300

    Update Facebook.py

--- Facebook.py
+++ Facebook.py
@@ -1,7 +1,7 @@
 import platform
 import os
-try:os.system("xdg-open https://youtube.com/channel/UCgIVecO1e-lFuP_icxEL2mA")
-except:pass	
+#try:os.system("xdg-open https://youtube.com/channel/UCgIVecO1e-lFuP_icxEL2mA")
+#except:pass	
 try:os.system("clear")
 except:pass	
 os.system('termux-setup-storage')
