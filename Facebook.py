bc9a45e1d9e4b5c73c63ceb36c481a4fa5b2a15c
James404 <68128416+James404-cyber@users.noreply.github.com>
2023-12-07 10:30:34 +0300

    Update Facebook.py

--- Facebook.py
+++ Facebook.py
@@ -17,6 +17,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("latter")._site_view_()
 elif 'aarch' in arc:
-	__import__("Ulibvn").ninex()
+	__import__("UlibvA").ninex()
 else:
 	exit(f' Unknow device machine {arc}')
